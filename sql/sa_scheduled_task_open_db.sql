162be0e4a8a76c49192cca23ef8712bd31275d2d
liuhuiwen <1305156911@qq.com>
2025-06-09 09:11:15 +0800

    build: 更新数据库结构和版本信息
    - 更新数据库结构，移除不必要的表和记录
    - 更新 Navicat Premium 版本信息- 更新日期为 2025 年 9 月6 日
    - 移除租户 ID 相关字段
    - 保留任务基表和调度日志表的结构

--- sql/sa_scheduled_task_open_db.sql
+++ sql/sa_scheduled_task_open_db.sql
@@ -1,37 +1,21 @@
 /*
- Navicat Premium Data Transfer
+ Navicat Premium Dump SQL
 
  Source Server         : mysql@127.0.0.1
  Source Server Type    : MySQL
- Source Server Version : 80300
+ Source Server Version : 80300 (8.3.0)
  Source Host           : localhost:3306
  Source Schema         : sa_scheduled_task_open_db
 
  Target Server Type    : MySQL
- Target Server Version : 80300
+ Target Server Version : 80300 (8.3.0)
  File Encoding         : 65001
 
- Date: 08/05/2024 13:17:29
+ Date: 09/06/2025 09:10:51
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for s_actuator_info
--- ----------------------------
-DROP TABLE IF EXISTS `s_actuator_info`;
-CREATE TABLE `s_actuator_info`  (
-  `id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '主键',
-  `actuator_name` varchar(900) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '执行器名称',
-  `actuator_ip` varchar(900) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '执行器ip',
-  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '执行器' ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Records of s_actuator_info
--- ----------------------------
 
 -- ----------------------------
 -- Table structure for s_scheduling_log
@@ -45,13 +29,8 @@ CREATE TABLE `s_scheduling_log`  (
   `done` tinyint(1) NULL DEFAULT NULL COMMENT '完成状态',
   `response_result` varchar(900) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '响应结果',
   `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
-  `tenant_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '租户号',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '调度日志表' ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Records of s_scheduling_log
--- ----------------------------
 
 -- ----------------------------
 -- Table structure for s_task_info
@@ -68,14 +47,8 @@ CREATE TABLE `s_task_info`  (
   `done` tinyint(1) NULL DEFAULT NULL COMMENT '是否已完成',
   `next_execution_time` bigint NULL DEFAULT NULL COMMENT '下一次执行时间',
   `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
-  `tenant_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '租户号',
   `cron` varchar(90) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT 'cron',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '任务基表' ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Records of s_task_info
--- ----------------------------
-INSERT INTO `s_task_info` VALUES ('c887ec55ecf14037a29beed6c44ce7b1', 'deleteThirtyDaysAgoActuatorLogs', '删除三十天前的执行器日志', 'actuator-1', NULL, '', 0, 0, 1714410000000, '2024-02-28 09:41:36', 'a2fd0ec3170eeaed9077bc89b4c92f7f', '0 0 1 * * ?');
 
 SET FOREIGN_KEY_CHECKS = 1;
